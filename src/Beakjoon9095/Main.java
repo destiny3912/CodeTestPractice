b1705105c809a221b07d2af8d731a41cfdfa4f75
Lee JIHO <vc54@naver.com>
2022-01-18 17:03:16 +0900

    Beakjoon9095

--- src/Beakjoon9095/Main.java
+++ src/Beakjoon9095/Main.java
@@ -1,5 +1,48 @@
 package Beakjoon9095;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
-    
+
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        int[] dp = new int[11];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+
+        int T = Integer.parseInt(stdin.readLine());
+        while(T > 0)
+        {
+            int N = Integer.parseInt(stdin.readLine());
+
+            for(int index = 1; index < N + 1; index++)
+            {
+                if(dp[index] != -1)
+                {
+                    continue;
+                }
+                else if(index >= 4)
+                {
+                    dp[index] = dp[index - 1] + dp[index - 2] +  dp[index - 3];
+                }
+                else
+                {
+                    if(index == 1)
+                    {
+                        dp[index] = 1;
+                    }
+                    else
+                    {
+                        dp[index] = dp[index - 1] * 2;
+                    }
+                }
+            }
+            System.out.println(dp[N]);
+            T--;
+        }
+    }
 }
